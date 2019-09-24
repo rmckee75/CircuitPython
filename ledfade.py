2e9e567c606c3199ee380111567322e217575486
Reece V Mckee <rmckee75@ccs.local>
2019-09-24 10:02:29 -0400

    Commented LED fade code

--- ledfade.py
+++ ledfade.py
@@ -2,19 +2,27 @@
 import board
 import pulseio
 import time
+# most functions must be imported from modules before they can be used
 led = pulseio.PWMOut(board.D4)
+# the LED is attached to digital pin 4 (a PWM pin)
 i = 0
+# duty_cycle starts at 0...
 j = 10
+# ...and goes up by 10
 print("Make it red!")
 
 while True:
+# equivalent of void loop ()
     led.duty_cycle = i
+    # the duty_cycle is the brightness
     time.sleep(0.0000001)
+    # wait a very small amount of time
     print (i)
     i = i + j
+    # brightness (duty_cycle) changes by 10 each time
     if (i == 65530 or i == 0):
         j = -j
-
+        # when brightness reaches 0 or 65530 (max value), switch directions
 
 
 

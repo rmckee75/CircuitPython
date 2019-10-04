31a31e90e4b519a3d6bac1f16f0f4a41c8a15686
Reece V Mckee <rmckee75@ccs.local>
2019-10-04 09:33:43 -0400

    Added code for RGB assignment

--- classesobjectsmodules.py
+++ classesobjectsmodules.py
@@ -1,6 +1,7 @@
+# you must make an RGB module to make this code run
 import time
 import board
-from rgb import RGB   # import the RGB class from the rgb module
+from rgb import RGB  # import the RGB class from the rgb module
 
 r1 = board.D3
 g1 = board.D4
@@ -8,9 +9,9 @@ b1 = board.D5
 r2 = board.D2
 g2 = board.D11
 b2 = board.D12
-
+# some pins share timers, so switch them around as necessary
 myRGB1 = RGB(r1,g1,b1)   # create a new RGB object, using pins 3, 4, and 5
-myRGB2 = RGB(r2,g2,b2)   # create a new RGB object, using pins 8, 9, and 10
+myRGB2 = RGB(r2,g2,b2)   # create a new RGB object, using pins 2, 11, and 12
 while True:
     myRGB1.red()          # Glow red
     myRGB2.green()        # Glow green
--- rgb.py
+++ rgb.py
@@ -1,13 +1,17 @@
 from pulseio import PWMOut
+# import the PWMOut class from the pulseio module
 class RGB(object):
     def __init__ (self, Rpin, Gpin, Bpin):
+    # the object (RGB LED) is initialized with a a red, green, and blue pins
         self.Rpin = PWMOut (Rpin, duty_cycle = 65535, frequency = 500)
         self.Gpin = PWMOut (Gpin, duty_cycle = 65535, frequency = 500)
         self.Bpin = PWMOut (Bpin, duty_cycle = 65535, frequency = 500)
+        # each pin is a PWM pin, and they all start off
     def red (self):
         self.Rpin.duty_cycle = 0
         self.Gpin.duty_cycle = 65535
         self.Bpin.duty_cycle = 65535
+        # when there is power, the pins defualt value is on, so turning them to 65535 turns them off, not on
     def blue (self):
         self.Rpin.duty_cycle = 65535
         self.Gpin.duty_cycle = 65535
@@ -17,14 +21,17 @@ class RGB(object):
         self.Gpin.duty_cycle = 0
         self.Bpin.duty_cycle = 65535
     def cyan (self):
+        # green + blue
         self.Rpin.duty_cycle = 65535
         self.Gpin.duty_cycle = 0
         self.Bpin.duty_cycle = 0
     def magenta (self):
+        # red + blue
         self.Rpin.duty_cycle = 0
         self.Gpin.duty_cycle = 65535
         self.Bpin.duty_cycle = 0
     def yellow (self):
+        # red + green
         self.Rpin.duty_cycle = 0
         self.Gpin.duty_cycle = 0
         self.Bpin.duty_cycle = 65535
